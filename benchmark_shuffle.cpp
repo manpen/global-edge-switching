164b13464df034f47cceef257326fcca0f02e1a5
Daniel Allendorf <daniel@ae.cs.uni-frankfurt.de>
2021-08-02 14:09:31 +0200

    Revert "add naive shuffle for comparison"
    This reverts commit e8d21b260ca8d89f4e9a4a31b9ca2c3e999f72fc.

--- benchmark_shuffle.cpp
+++ benchmark_shuffle.cpp
@@ -24,37 +24,11 @@ void benchmark_shuffle(node_t n, edge_t target_m, size_t num_threads, std::mt199
     });
 
     omp_set_num_threads(num_threads);
-    std::vector<edge_t> edge_list1 = edge_list;
     {
         incpwl::ScopedTimer timer;
-        shuffle::GeneratorProvider gen_prov(gen, num_threads);
-        shuffle::parallel::iss_shuffle(edge_list1.begin(), edge_list1.end(), gen_prov);
+        shuffle::GeneratorProvider gen_prov(gen);
+        shuffle::parallel::iss_shuffle(edge_list.begin(), edge_list.end(), gen_prov);
         std::cout << "PUs: " << num_threads << ": Runtime " << timer.elapsedSeconds() << "s\n";
-    }
-    std::vector<std::mt19937_64> gen_local(num_threads, std::mt19937_64(gen()));
-    std::vector<edge_t> edge_list2 = edge_list;
-    {
-        incpwl::ScopedTimer timer;
-        size_t r = num_threads;
-        std::shuffle(edge_list2.begin(), edge_list2.begin() + r, gen);
-        while (r < edge_list2.size() / 2) {
-            #pragma omp parallel num_threads(num_threads)
-            {
-                const size_t thread_id = omp_get_thread_num();
-
-                std::mt19937_64 &gen = gen_local[thread_id];
-                shuffle::RandomBits fair_coin;
-
-                const size_t block_size = r / num_threads;
-                const size_t beg = block_size * thread_id;
-                const size_t end = beg + block_size;
-                for (size_t i = beg; i < end; ++i) {
-                    if (fair_coin(gen)) std::swap(edge_list2[i], edge_list2[r + i]);
-                }
-            }
-            r *= 2;
-        }
-        std::cout << "PUs: " << num_threads << ": Runtime2 " << timer.elapsedSeconds() << "s\n";
     }
 }
 
@@ -62,10 +36,10 @@ int main() {
     std::mt19937_64 gen{0};
 
     node_t n = 1<<20;
-    edge_t target_m = n * 50.44;
+    edge_t target_m = n * 1.44;
 
-    for (int repeat = 0; repeat < 1; ++repeat) {
-        for (size_t num_threads = 2; num_threads <= 8; num_threads += 2) {
+    for (int repeat = 0; repeat < 5; ++repeat) {
+        for (size_t num_threads = 2; num_threads <= 32; num_threads += 2) {
             benchmark_shuffle(n, target_m, num_threads, gen);
         }
         std::cout << "\n";
