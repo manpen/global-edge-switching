e355cf5dc4d900afec5da6da0eddab590db28e44
Daniel Allendorf <daniel@ae.cs.uni-frankfurt.de>
2021-08-24 10:56:55 +0200

    strange bug

--- include/es/EdgeDependencies.hpp
+++ include/es/EdgeDependencies.hpp
@@ -12,6 +12,10 @@ struct EdgeDependencies {
 public:
     EdgeDependencies(size_t num_edges, double load_factor) : dependencies_(load_factor * 2 * num_edges), round_(0) {}
 
+    void next_round() {
+        round_++;
+    }
+
     void announce_erase(edge_t edge, size_t sid) {
         auto [iter, _] = insert_or_find(edge);
         iter->erasing_switch = sid;
@@ -59,9 +63,46 @@ public:
         }
     }
 
+    struct EdgeDependency {
+        edge_t edge = kEmpty_;
+        bool resolved = true;
+        size_t erasing_switch = 0;
+        std::vector<std::pair<size_t, bool>> inserting_switches;
+        std::atomic<bool> unlocked = true;
+        std::atomic<int> round = -1;
+    };
+
+    using iterator_t = typename std::vector<EdgeDependency>::iterator;
+    using const_iterator_t = typename std::vector<EdgeDependency>::const_iterator;
+
+    size_t find_existing2(edge_t edge) {
+        size_t bucket = hash_func_(edge) % dependencies_.size();
+        auto iter = dependencies_.begin() + bucket;
+        while (true) {
+            if (iter->edge == edge) return std::distance(dependencies_.begin(), iter);
+            iter++;
+            if (iter == dependencies_.end()) iter = dependencies_.begin();
+        }
+    }
+
+    iterator_t find_existing(edge_t edge) {
+        size_t bucket = hash_func_(edge) % dependencies_.size();
+        auto iter = dependencies_.begin() + bucket;
+        while (true) {
+            if (iter->edge == edge) return iter;
+            iter++;
+            if (iter == dependencies_.end()) iter = dependencies_.begin();
+        }
+    }
+
     std::pair<size_t, bool> lookup_erase(edge_t edge) const {
         const auto iter = find_existing(edge);
         return {iter->erasing_switch, iter->resolved};
+    }
+
+    std::pair<size_t, bool> lookup_erase2(size_t edge_pos) const {
+        auto& dep = dependencies_[edge_pos];
+        return {dep.erasing_switch, dep.resolved};
     }
 
     std::pair<size_t, bool> lookup_insert(edge_t edge) const {
@@ -77,30 +118,14 @@ public:
         return {earliest, resolved};
     }
 
-    void next_round() {
-        round_++;
-    }
-
+    const static size_t kNone_ = std::numeric_limits<size_t>::max();
 private:
     const static edge_t kEmpty_ = std::numeric_limits<edge_t>::max();
-    const static size_t kNone_ = std::numeric_limits<size_t>::max();
     const static int kLocked_ = std::numeric_limits<int>::max();
-
-    struct EdgeDependency {
-        edge_t edge = kEmpty_;
-        bool resolved = true;
-        size_t erasing_switch = 0;
-        std::vector<std::pair<size_t, bool>> inserting_switches;
-        std::atomic<bool> unlocked = true;
-        std::atomic<int> round = -1;
-    };
 
     std::vector<EdgeDependency> dependencies_;
     HashFcn hash_func_;
     int round_;
-
-    using iterator_t = typename std::vector<EdgeDependency>::iterator;
-    using const_iterator_t = typename std::vector<EdgeDependency>::const_iterator;
 
     std::pair<iterator_t, bool> insert_or_find(edge_t edge) {
         size_t bucket = hash_func_(edge) % dependencies_.size();
@@ -121,18 +146,9 @@ private:
                     return {iter, true};
                 }
             }
-            if (iter->round.load(std::memory_order_acquire) == kLocked_) continue;
+            round_at_iter = iter->round.load(std::memory_order_acquire);
+            if (round_at_iter == kLocked_ || round_at_iter < round_) continue;
             if (iter->edge == edge)  return {iter, false};
-            iter++;
-            if (iter == dependencies_.end()) iter = dependencies_.begin();
-        }
-    }
-
-    iterator_t find_existing(edge_t edge) {
-        size_t bucket = hash_func_(edge) % dependencies_.size();
-        auto iter = dependencies_.begin() + bucket;
-        while (true) {
-            if (iter->edge == edge)  return iter;
             iter++;
             if (iter == dependencies_.end()) iter = dependencies_.begin();
         }
