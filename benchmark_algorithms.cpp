6692e8dc0c13dfc90016615855615f217c353f6a
Daniel Allendorf <daniel@ae.cs.uni-frankfurt.de>
2021-09-10 15:58:33 +0200

    write/read initial graphs from files

--- benchmark_algorithms.cpp
+++ benchmark_algorithms.cpp
@@ -31,8 +31,86 @@
 
 using namespace es;
 
-void run_benchmark(std::string_view algo, NetworKit::Graph graph, std::mt19937_64& gen,
-                   size_t switches_per_edge = 10, bool detailed = true) {
+NetworKit::Graph read_graph(std::string filename) {
+    std::ifstream ifile{filename};
+    std::string line;
+    getline(ifile, line);
+    node_t n = std::stoll(line.substr(2));
+    NetworKit::Graph graph(n);
+    while (getline(ifile, line)) {
+        if (line.starts_with('%')) continue;
+        size_t sep = line.find(',');
+        node_t u = std::stoll(line.substr(0, sep));
+        node_t v = std::stoll(line.substr(sep + 1));
+        graph.addEdge(u, v);
+    }
+    return graph;
+}
+
+void write_graph(const NetworKit::Graph& graph, std::string filename) {
+    std::ofstream ofile{filename};
+    ofile << "% " << graph.numberOfNodes() << "\n";
+    ofile << "% " << graph.numberOfEdges() << "\n";
+    graph.forEdges([&](NetworKit::node u, NetworKit::node v){
+        ofile << u << "," << v << "\n";
+    });
+    ofile << std::flush;
+}
+
+void generate_random_graph(int argc, const char** argv) {
+    if (argc < 3)
+        throw std::runtime_error("Missing output filename");
+    std::string filename{argv[2]};
+    node_t n = argc > 3 ? (1<<std::stoi(argv[3])) : 1<<20;
+    edge_t target_m = argc > 4 ? n * std::stod(argv[4]) : n * 1.44;
+
+    double p = (2.0 * target_m) / n / (n - 1);
+    auto graph = NetworKit::ErdosRenyiGenerator(n, p, false, false).generate();
+    write_graph(graph, filename);
+}
+
+void generate_powerlaw_graph(int argc, const char** argv) {
+    if (argc < 3)
+        throw std::runtime_error("Missing output filename");
+    std::string filename{argv[2]};
+    node_t n = argc > 3 ? (1<<std::stoi(argv[3])) : 1<<20;
+    double gamma = argc > 4 ? std::stod(argv[4]) : 2.5;
+    edge_t d_min = argc > 5 ? std::stod(argv[5]) : 1;
+
+    NetworKit::PowerlawDegreeSequence ds_gen(d_min, n - 1, -gamma);
+    std::vector<NetworKit::count> ds;
+    bool realizable;
+    do {
+        ds_gen.run();
+        ds = ds_gen.getDegreeSequence(n);
+        realizable = NetworKit::HavelHakimiGenerator(ds).isRealizable();
+    } while (!realizable);
+    auto graph = NetworKit::HavelHakimiGenerator(ds).generate();
+    write_graph(graph, filename);
+}
+
+void benchmark_on_file(int argc, const char** argv) {
+    if (argc < 3)
+        throw std::runtime_error("Missing filename");
+    std::string filename{argv[2]};
+    auto graph = read_graph(filename);
+    size_t threads = argc > 3 ? std::stoi(argv[3]) : 4;
+    size_t repeats = argc > 4 ? std::stoi(argv[4]) : 1;
+    std::string algo = argc > 5 ? argv[5] : "global-no-wait";
+    size_t switches = argc > 6 ? std::stoi(argv[6]) : 10;
+    bool detailed = argc > 7 ? std::string(argv[7]) == "verbose" : false;
+
+    std::cout << "Starting experiment with parameters" << std::endl
+              << "algo=" << algo << std::endl
+              << "switches=" << switches << "m" << std::endl
+              << "file=" << filename << std::endl
+              << "n=" << graph.numberOfNodes() << std::endl
+              << "m=" << graph.numberOfEdges() << std::endl
+              << "p=" << threads << std::endl
+              << "repeats=" << repeats << std::endl << std::endl;
+
+    omp_set_num_threads(threads);
+
     std::unique_ptr<AlgorithmBase> es;
     if (algo == "robin") {
         es = std::make_unique<AlgorithmVectorSet<tsl::robin_set<edge_t, edge_hash_crc32>>>(graph);
@@ -46,144 +124,27 @@ void run_benchmark(std::string_view algo, NetworKit::Graph graph, std::mt19937_6
         es = std::make_unique<AlgorithmParallelGlobalNoWaitV2>(graph);
     }
 
-    {
+    std::mt19937_64 gen{0};
+    while (repeats--) {
         incpwl::ScopedTimer timer;
         const edge_t m = graph.numberOfEdges();
-        const auto requested_switches = switches_per_edge * m;
+        const auto requested_switches = switches * m;
         const auto sucessful_switches = es->do_switches(gen, requested_switches);
         if (detailed) {
             std::cout << "Switches successful: " << (100. * sucessful_switches / requested_switches) << "% \n";
             std::cout << "Runtime: " << timer.elapsedSeconds() << "s\n";
             std::cout << "Switches per second: " << requested_switches / timer.elapsedSeconds() * 1e-6 << "M" << std::endl;
         }
-        std::cout << "Estimated randomization time: " << timer.elapsedSeconds() * (1. * requested_switches / sucessful_switches) << "s \n";
-    }
-}
-
-void benchmark_file(int argc, const char** argv) {
-    if (argc < 3)
-        throw std::runtime_error("Missing filename");
-    std::string filename{argv[2]};
-    double gamma = -1;
-    edge_t m = 0;
-    std::vector<edge_t> degree_sequence;
-    {
-        std::ifstream ifile{filename};
-        std::string line;
-        getline(ifile, line);
-        gamma = std::stod(line);
-        while (getline(ifile, line)) {
-            edge_t degree = std::stoul(line);
-            degree_sequence.push_back(degree);
-            m += degree;
-        }
-        if (!std::is_sorted(degree_sequence.begin(), degree_sequence.end(), std::greater<edge_t>()))
-            throw std::runtime_error("Degree sequence is not sorted");
-    }
-    if (!NetworKit::HavelHakimiGenerator(degree_sequence).isRealizable())
-        throw std::runtime_error("Degree sequence not realizable");
-    size_t threads = argc > 3 ? std::stoi(argv[3]) : 4;
-    size_t repeats = argc > 4 ? std::stoi(argv[4]) : 1;
-    std::string algo = argc > 5 ? argv[5] : "global-no-wait";
-    size_t switches = argc > 6 ? std::stoi(argv[6]) : 10;
-    bool detailed = argc > 7 ? std::string(argv[7]) == "verbose" : false;
-
-    std::cout << "Starting file experiment with parameters" << std::endl
-              << "algo=" << algo << std::endl
-              << "switches=" << switches << "m" << std::endl
-              << "file=" << filename << std::endl
-              << "n=" << degree_sequence.size() << std::endl
-              << "m=" << m << std::endl
-              << "gamma_estimate=" << gamma << std::endl
-              << "p=" << threads << std::endl
-              << "repeats=" << repeats << std::endl << std::endl;
-
-    omp_set_num_threads(threads);
-
-    std::mt19937_64 gen{0};
-    while (repeats--) {
-        auto graph = NetworKit::HavelHakimiGenerator(degree_sequence).generate();
-
-        run_benchmark(algo, graph, gen, switches, detailed);
-
-        std::cout << "\n";
-    }
-}
-
-void benchmark_random(int argc, const char** argv) {
-    node_t n = argc > 2 ? (1<<std::stoi(argv[2])) : 1<<20;
-    edge_t target_m = argc > 3 ? n * std::stod(argv[3]) : n * 1.44;
-    size_t threads = argc > 4 ? std::stoi(argv[4]) : 4;
-    size_t repeats = argc > 5 ? std::stoi(argv[5]) : 1;
-    std::string algo = argc > 6 ? argv[6] : "global-no-wait";
-    size_t switches = argc > 7 ? std::stoi(argv[7]) : 10;
-    bool detailed = argc > 8 ? std::string(argv[8]) == "verbose" : false;
-
-    std::cout << "Starting random-graph experiment with parameters" << std::endl
-              << "algo=" << algo << std::endl
-              << "switches=" << switches << "m" << std::endl
-              << "n=" << n << std::endl
-              << "m=" << target_m << std::endl
-              << "p=" << threads << std::endl
-              << "repeats=" << repeats << std::endl << std::endl;
-
-    omp_set_num_threads(threads);
-
-    std::mt19937_64 gen{0};
-    while (repeats--) {
-        double p = (2.0 * target_m) / n / (n - 1);
-        auto graph = NetworKit::ErdosRenyiGenerator(n, p, false, false).generate();
-
-        run_benchmark(algo, graph, gen, switches, detailed);
-
-        std::cout << "\n";
-    }
-}
-
-void benchmark_powerlaw(int argc, const char** argv) {
-    node_t n = argc > 2 ? (1<<std::stoi(argv[2])) : 1<<20;
-    double gamma = argc > 3 ? std::stod(argv[3]) : 2.5;
-    edge_t d_min = argc > 4 ? std::stod(argv[4]) : 1;
-    size_t threads = argc > 5 ? std::stoi(argv[5]) : 4;
-    size_t repeats = argc > 6 ? std::stoi(argv[6]) : 1;
-    std::string algo = argc > 7 ? argv[7] : "global-no-wait";
-    size_t switches = argc > 8 ? std::stoi(argv[8]) : 10;
-    bool detailed = argc > 9 ? std::string(argv[9]) == "verbose" : false;
-
-    std::cout << "Starting powerlaw-graph experiment with parameters" << std::endl
-              << "algo=" << algo << std::endl
-              << "switches=" << switches << "m" << std::endl
-              << "n=" << n << std::endl
-              << "gamma=" << std::setprecision(3) << gamma << std::endl
-              << "min-degree=" << d_min << std::endl
-              << "p=" << threads << std::endl
-              << "repeats=" << repeats << std::endl << std::endl;
-
-    omp_set_num_threads(threads);
-
-    std::mt19937_64 gen{0};
-    while (repeats--) {
-        NetworKit::PowerlawDegreeSequence ds_gen(d_min, n - 1, -gamma);
-        std::vector<NetworKit::count> ds;
-        bool realizable;
-        do {
-            ds_gen.run();
-            ds = ds_gen.getDegreeSequence(n);
-            realizable = NetworKit::HavelHakimiGenerator(ds).isRealizable();
-        } while (!realizable);
-        auto graph = NetworKit::HavelHakimiGenerator(ds).generate();
-
-        run_benchmark(algo, graph, gen, switches, detailed);
-
+        std::cout << "Runtime for 1m successful switches: " << timer.elapsedSeconds() * (1. * m / sucessful_switches) << "s \n";
         std::cout << "\n";
     }
 }
 
 int main(int argc, const char** argv) {
-    std::string mode = argc > 1 ? argv[1] : "random";
-    if (mode == "random") benchmark_random(argc, argv);
-    else if (mode == "powerlaw") benchmark_powerlaw(argc, argv);
-    else if (mode == "file") benchmark_file(argc, argv);
+    std::string mode = argc > 1 ? argv[1] : "bench";
+    if (mode == "bench") benchmark_on_file(argc, argv);
+    else if (mode == "generate-random") generate_random_graph(argc, argv);
+    else if (mode == "generate-powerlaw") generate_powerlaw_graph(argc, argv);
 
     return 0;
 }
