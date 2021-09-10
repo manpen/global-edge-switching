8a48f430bfa9daa4b66806c84fc7c3863e5d720c
Daniel Allendorf <daniel@ae.cs.uni-frankfurt.de>
2021-09-10 16:50:34 +0200

    improve benchmark output some more

--- benchmark_algorithms.cpp
+++ benchmark_algorithms.cpp
@@ -142,12 +142,12 @@ void benchmark_on_file(int argc, const char** argv) {
             std::cout << "Initialization time: " << init_time << "s \n";
             std::cout << "Runtime + Initialization: " << run_time + init_time << "s\n";
             std::cout << "Switches per second: " << requested_switches / run_time * 1e-6 << "M \n";
-            std::cout << "Switches per second + Initialization: " << requested_switches / (run_time + init_time) * 1e-6 << "M \n";
             std::cout << "Successful switches per second: " << sucessful_switches / run_time * 1e-6 << "M \n";
-            std::cout << "Successful switches per second + Initialization: " << sucessful_switches / (run_time + init_time) * 1e-6 << "M \n";
+            std::cout << "Runtime for 1m successful switches: " << run_time * (1. * m / sucessful_switches) << "s \n";
+            std::cout << "Runtime for 1m successful switches + Initialization: " << init_time + run_time * (1. * m / sucessful_switches) << "s \n";
         }
-        std::cout << "Runtime for 1m successful switches: " << run_time * (1. * m / sucessful_switches) << "s \n";
-        if (detailed) std::cout << "Runtime for 1m successful switches + Initialization: " << init_time + run_time * (1. * m / sucessful_switches) << "s \n";
+        std::cout << "Runtime for 10m successful switches: " << run_time * (1. * switches * m / sucessful_switches) << "s \n";
+        std::cout << "Runtime for 10m successful switches + Initialization: " << init_time + run_time * (1. * switches * m / sucessful_switches) << "s \n";
         std::cout << std::endl;
     }
 }
