5a0a8ae5582c48c05682dcdcb6659f84fccccf51
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2021-09-17 00:40:22 +0200

    Add reporting of true value of n, improve job generator

--- autocorrelation/generate_jobs.py
+++ autocorrelation/generate_jobs.py
@@ -1,18 +1,20 @@
-boilerplate = "" \
-+ "#!/bin/bash\n" \
-+ "#SBATCH --job-name=autocorr\n" \
-+ "#SBATCH --partition=general1\n" \
-+ "#SBATCH --ntasks=1\n" \
-+ "#SBATCH --cpus-per-task=16\n" \
-+ "#SBATCH --mem-per-cpu=3500\n" \
-+ "#SBATCH --time=48:00:00\n" \
-+ "#SBATCH --no-requeue\n" \
-+ "#SBATCH --mail-type=FAIL\n" \
-+ "\n" \
-+ "DIR=\"/scratch/memhierarchy/penschuck/networks/network-repository.com/\"\n" \
-+ "OUTPUTDIR=\"/scratch/memhierarchy/tran/autocorrelation/$SLURM_JOB_ID/\"\n" \
-+ "mkdir -p $OUTPUTDIR\n" \
-+ "cp /home/memhierarchy/tran/edge-switching/release/autocorrelation_realworld .\n"
+def return_boilerplate(jobid):
+    boilerplate = "" \
+        + "#!/bin/bash\n" \
+        + "#SBATCH --job-name=acorr" + str(jobid) + "\n" \
+        + "#SBATCH --partition=general1\n" \
+        + "#SBATCH --ntasks=1\n" \
+        + "#SBATCH --cpus-per-task=16\n" \
+        + "#SBATCH --mem-per-cpu=3500\n" \
+        + "#SBATCH --time=48:00:00\n" \
+        + "#SBATCH --no-requeue\n" \
+        + "#SBATCH --mail-type=FAIL\n" \
+        + "\n" \
+        + "DIR=\"/scratch/memhierarchy/penschuck/networks/network-repository.com/\"\n" \
+        + "OUTPUTDIR=\"/scratch/memhierarchy/tran/autocorrelation/$SLURM_JOB_ID/\"\n" \
+        + "mkdir -p $OUTPUTDIR\n" \
+        + "cp /home/memhierarchy/tran/edge-switching/release/autocorrelation_realworld .\n"
+    return boilerplate
 
 inputdir = "/scratch/memhierarchy/penschuck/networks/network-repository.com"
 outputdir = "/scratch/memhierarchy/tran/autocorrelation"
@@ -24,7 +26,7 @@ L = []
 with open("sorted.csv", "r") as reader:
     for line in reader.read().splitlines()[1::]:
         s = line.split(",")
-        if int(s[2]) >= 100 and int(s[1]) <= 100000 and int(s[2]) <= 5000000:
+        if int(s[2]) >= 100 and int(s[1]) <= 5000 and int(s[2]) <= 5000000:
             L.append((s[0], int(s[1]), int(s[2])))
     
 L.sort(key=lambda x: x[2])
@@ -43,8 +45,10 @@ for t in L:
 
 for Jobgraphs, index in zip(J, range(len(J))):
     with open("job-{}.sh".format(index), "w") as writer:
-        writer.write(boilerplate)
+        writer.write(return_boilerplate(index))
+        if len(Jobgraphs):
+            continue
         for graph in Jobgraphs:
             log_fn = graph[0].split("/")[-1].split(".simp-undir-edges")[0]
-            writer.write("./autocorrelation_realworld 1 {}/{} 20 1 2 3 4 5 6 8 9 10 12 14 15 16 18 20 30 40 50 --minsnaps {} --maxsnaps {} --pus {} >> {}/$SLURM_JOB_ID/{}-1.log\n".format(inputdir, graph[0], minsnaps, maxsnaps, pus, outputdir, log_fn))
-            writer.write("./autocorrelation_realworld 2 {}/{} 20 1 2 3 4 5 6 8 9 10 12 14 15 16 18 20 30 40 50 --minsnaps {} --maxsnaps {} --pus {} >> {}/$SLURM_JOB_ID/{}-2.log\n".format(inputdir, graph[0], minsnaps, maxsnaps, pus, outputdir, log_fn))
+            writer.write("./autocorrelation_realworld 1 {}/{} 20 1 2 3 4 5 6 8 9 10 12 14 15 18 20 30 40 --minsnaps {} --maxsnaps {} --pus {} >> {}/$SLURM_JOB_ID/{}-1.log\n".format(inputdir, graph[0], minsnaps, maxsnaps, pus, outputdir, log_fn))
+            writer.write("./autocorrelation_realworld 2 {}/{} 20 1 2 3 4 5 6 8 9 10 12 14 15 18 20 30 40 --minsnaps {} --maxsnaps {} --pus {} >> {}/$SLURM_JOB_ID/{}-2.log\n".format(inputdir, graph[0], minsnaps, maxsnaps, pus, outputdir, log_fn))
