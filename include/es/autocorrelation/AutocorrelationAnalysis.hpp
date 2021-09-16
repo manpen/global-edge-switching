05ce32521af44b60bd16c3b5ccf1b373935c9b9a
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2021-09-16 16:21:15 +0200

    Change thinnings from m -> m/2

--- include/es/autocorrelation/AutocorrelationAnalysis.hpp
+++ include/es/autocorrelation/AutocorrelationAnalysis.hpp
@@ -161,7 +161,7 @@ public:
 
             // compute requested number of switches, filling the gap from last snapshot to this snapshot
             const auto factor = snapshot - last_snapshot;
-            const auto requested_switches = factor * switches_per_edge * graph.numberOfEdges();
+            const auto requested_switches = factor * switches_per_edge * graph.numberOfEdges() / 2;
 
             Algo es(curr_graph);
             successful_switches[snapshotid] = es.do_switches(gen, requested_switches, true);
