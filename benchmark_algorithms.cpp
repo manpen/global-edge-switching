59a88ac032300a191079f995bea8d139fe3a940d
Daniel Allendorf <daniel@ae.cs.uni-frankfurt.de>
2021-09-27 10:26:29 +0200

    add old global-no-wait to benchmarks for comparison

--- benchmark_algorithms.cpp
+++ benchmark_algorithms.cpp
@@ -22,6 +22,7 @@
 #include <es/algorithms/AlgorithmParallelNaive.hpp>
 #include <es/algorithms/AlgorithmParallelNaiveGlobal.hpp>
 #include <es/algorithms/AlgorithmParallelGlobal.hpp>
+#include <es/algorithms/AlgorithmParallelGlobalNoWaitV2.hpp>
 #include <es/algorithms/AlgorithmParallelGlobalNoWaitV4.hpp>
 #include <es/algorithms/AlgorithmVectorRobin.hpp>
 #include <es/algorithms/AlgorithmNetworKit.hpp>
@@ -135,6 +136,8 @@ void benchmark_on_file(int argc, const char** argv) {
             es = std::make_unique<AlgorithmParallelGlobal>(graph);
         } else if (algo == "global-no-wait") {
             es = std::make_unique<AlgorithmParallelGlobalNoWaitV4>(graph);
+        } else if (algo == "global-no-wait-old") {
+            es = std::make_unique<AlgorithmParallelGlobalNoWaitV2>(graph);
         } else if (algo == "networkit") {
             es = std::make_unique<AlgorithmNetworKit>(graph);
         } else if (algo == "gengraph") {
