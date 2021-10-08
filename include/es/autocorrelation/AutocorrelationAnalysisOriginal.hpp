9daac70448bc9b27bb878158e9874424de459a2c
Hung Tran <hung@ae.cs.uni-frankfurt.de>
2021-10-08 05:52:56 +0200

    Chop up Switches for RobinES

--- include/es/autocorrelation/AutocorrelationAnalysisOriginal.hpp
+++ include/es/autocorrelation/AutocorrelationAnalysisOriginal.hpp
@@ -180,12 +180,30 @@ public:
 
             // compute requested number of switches, filling the gap from last snapshot to this snapshot
             const auto factor = snapshot - last_snapshot;
-            const auto requested_switches = factor * switches_per_edge * graph.numberOfEdges() / 2 + 1;
+            const auto requested_switches = factor * switches_per_edge * m/2 + 1;
 
             // perform switchings
-            Algo es(curr_graph);
-            successful_switches[snapshotid] = es.do_switches(gen, requested_switches, true);
-            const auto &edgelist = es.get_edgelist();
+            std::vector<es::edge_t> edgelist;
+            if (algo_label == "ES-Robin") {
+                auto todo_switches = requested_switches;
+                auto step = (factor * switches_per_edge == 1 ? m/2 + 1 : m/2);
+                while (todo_switches > 0) {
+                    const auto num_switches = std::min(step, todo_switches);
+                    Algo es(curr_graph);
+                    successful_switches[snapshotid] += es.do_switches(gen, num_switches, true);
+                    todo_switches -= num_switches;
+
+                    if (todo_switches == 0)
+                        edgelist = es.get_edgelist();
+                    
+                    curr_graph = es.get_graph();
+                }
+            } else {
+                Algo es(curr_graph);
+                successful_switches[snapshotid] = es.do_switches(gen, requested_switches, true);
+                edgelist = es.get_edgelist();
+                curr_graph = es.get_graph();
+            }
 
             // sort edgelist
             std::vector<std::pair<NetworKit::node, NetworKit::node>> pairs_edgelist;
@@ -264,7 +282,6 @@ public:
             }
 
             last_snapshot = snapshot;
-            curr_graph = es.get_graph();
         }
 
         std::cout << "# done switching " << output_fn_prefix << std::endl;
